e92ca3453bcb9d6a4bd005d9066a960c90257e14
Igor Barros <barrosigor74@gmail.com>
2022-03-11 03:27:29 -0300

    Atributos da classe Venda e Lista de Produtos.

--- projeto-java-oo/src/projeto/classe/Venda.java
+++ projeto-java-oo/src/projeto/classe/Venda.java
@@ -1,5 +1,89 @@
 package projeto.classe;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Venda {
+
+	private Long id;
+	private String descricaoVenda;
+	private String nomeCliente;
+	private String enderecoEntrega;
+
+	private BigDecimal ValorTotal;
+	
+	private List<Produto> listaprodutos = new ArrayList<Produto>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricaoVenda() {
+		return descricaoVenda;
+	}
+
+	public void setDescricaoVenda(String descricaoVenda) {
+		this.descricaoVenda = descricaoVenda;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getEnderecoEntrega() {
+		return enderecoEntrega;
+	}
+
+	public void setEnderecoEntrega(String enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public BigDecimal getValorTotal() {
+		return ValorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		ValorTotal = valorTotal;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Venda other = (Venda) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Venda [id=" + id + ", descricaoVenda=" + descricaoVenda + ", nomeCliente=" + nomeCliente
+				+ ", enderecoEntrega=" + enderecoEntrega + ", ValorTotal=" + ValorTotal + "]";
+	}
 
 }
