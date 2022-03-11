cb2f03a4b6063abb8751bbe3deab82de3f562ea6
Igor Barros <barrosigor74@gmail.com>
2022-03-11 03:51:55 -0300

    Implementação da classe ExecutarTeste. Gerar descrição da Venda.

--- projeto-java-oo/src/projeto/principal/ExecutarTeste.java
+++ projeto-java-oo/src/projeto/principal/ExecutarTeste.java
@@ -1,10 +1,21 @@
 package projeto.principal;
 
+import java.math.BigDecimal;
+
+import projeto.classe.Venda;
+
 public class ExecutarTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Venda venda = new Venda();
+		venda.setDescricaoVenda("Software");
+		venda.setEnderecoEntrega("Rua da Azeda, 85 ");
+		venda.setId(10L);
 
+		venda.setNomeCliente("Igor ");
+		venda.setValorTotal(BigDecimal.valueOf(666));
+		
+		System.out.println("Descrição da venda: " + venda.getDescricaoVenda());
 	}
-
 }
